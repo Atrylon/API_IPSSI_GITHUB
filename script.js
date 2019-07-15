4811c47ca68a7b0b1f9cba0b5af52a9aac22b664
Berenger Desgardin <berenger.desgardin@gmail.com>
2019-07-15 14:08:25 +0200

    modif affichage  + input

--- script.js
+++ script.js
@@ -14,8 +14,23 @@ app.appendChild(container);
 function getOrgName(){
     orgName=document.getElementById("org").value;
 
+    try{
+        document.getElementById("root").innerHTML = "";
+    }
+    catch(error){
+        console.log(error);
+    }
 
-fetch('https://api.github.com/orgs/'+orgName+'/members')
+    const logo = document.createElement('img');
+    logo.src = 'http://pngimg.com/uploads/github/github_PNG30.png';
+
+    const container = document.createElement('div');
+    container.setAttribute('class', 'container');
+
+    app.appendChild(logo);
+    app.appendChild(container);
+
+    fetch('https://api.github.com/orgs/'+orgName+'/members')
     .then(function(response) {
         return response.json();
     })
@@ -27,10 +42,14 @@ fetch('https://api.github.com/orgs/'+orgName+'/members')
                 const h1 = document.createElement('h1');
                 h1.textContent = members.login;
 
+                const avatar = document.createElement('img');
+                avatar.src = members.avatar_url;
+
                 const p = document.createElement('p');
-                p.textContent = `Y A RIEN...`;
+                p.textContent = `Pas d'infos disponibles...`;
 
                 container.appendChild(card);
+                card.appendChild(avatar);
                 card.appendChild(h1);
                 card.appendChild(p);
             });
@@ -40,37 +59,6 @@ fetch('https://api.github.com/orgs/'+orgName+'/members')
             errorMessage.textContent = `Gah, it's not working!`;
             app.appendChild(errorMessage);
     });
-//
-//     var request = new XMLHttpRequest();
-//     alert('https://api.github.com/orgs/'+orgName);
-//     request.open('GET', 'https://api.github.com/orgs/'+orgName+'/members', true);
-//     request.onload = function () {
-//
-//         // Begin accessing JSON data here
-//         var data = JSON.parse(this.response);
-//         if (request.status >= 200 && request.status < 400) {
-//             data.forEach(members => {
-//                 const card = document.createElement('div');
-//                 card.setAttribute('class', 'card');
-//
-//                 const h1 = document.createElement('h1');
-//                 h1.textContent = members.login;
-//
-//                 const p = document.createElement('p');
-//                 p.textContent = `Y A RIEN...`;
-//
-//                 container.appendChild(card);
-//                 card.appendChild(h1);
-//                 card.appendChild(p);
-//             });
-//         } else {
-//             const errorMessage = document.createElement('marquee');
-//             errorMessage.textContent = `Gah, it's not working!`;
-//             app.appendChild(errorMessage);
-//         }
-//     };
-//
-//     request.send();
 }
 
 
